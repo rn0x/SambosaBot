28e36aaa83233d5adba58d084538458d46d425f9
rn0x <rn0x.me@gmail.com>
2025-01-22 00:11:13 +0300

    🚀 2025-01-22 00:11

--- src/events/ready.mjs
+++ src/events/ready.mjs
@@ -2,7 +2,17 @@
 import client from '../client.mjs'
 
 export default function ready(client) {
-    client.on('ready', () => {
+    client.on('ready', async () => {
         console.log('WhatsApp bot is ready!');
+        // الحصول على جميع القروبات
+        const chats = await client.getChats();
+
+        // تصفية القروبات فقط
+        const groups = chats.filter(chat => chat.id._serialized.includes('@g.us'));
+
+        // طباعة معرف القروب لكل قروب
+        groups.forEach(group => {
+            console.log(`Group Name: ${group.name}, Group ID: ${group.id._serialized}`);
+        });
     });
 }
